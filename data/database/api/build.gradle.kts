a3ce426c1d132756ef7d53e18c54044657844207
wonseok <dnjstjr245@gmail.com>
2023-12-22 14:21:30 +0900

    Implemented Basic Structure for LocalDataSource in :data:database:api module

---
+++ data/database/api/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("kotlin")
+}
+
+dependencies {
+    // TODO :data:model
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.coroutines.core)
+}
---
+++ data/database/api/src/main/java/com/onseok/marvelpedia/database/LocalDataSource.kt
@@ -0,0 +1,5 @@
+package com.onseok.marvelpedia.database
+
+interface LocalDataSource {
+    // TODO Implement methods for local data operations
+}
